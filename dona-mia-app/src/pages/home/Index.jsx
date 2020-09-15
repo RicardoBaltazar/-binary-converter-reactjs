1ac9c08b897f3d0de4200aadb2e8b17704eec371
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-15 17:55:59 -0300

    feature - criando title da home-page

--- dona-mia-app/src/pages/home/Index.jsx
+++ dona-mia-app/src/pages/home/Index.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import './home.css'
+
+const IMG1 = require('../../assets/img-home-1.png')
+const IMG2 = require('../../assets/img-home-2.png')
+const IMG3 = require('../../assets/img-home-3.png')
 
 export default function Home() {
     return (
-        <h1>asdasd</h1>
+        <>
+            <main>
+                <section className='title'>
+                    <h1>DONA MIA</h1>
+                    <p>Um lugar como a sua casa</p>
+                    <button>Ver o Menu</button>
+
+                    <div className='images-title'>
+                        <img src={IMG1} alt="" />
+                        <img src={IMG2} alt="" />
+                        <img src={IMG3} alt="" />
+                    </div>
+                </section>
+
+            </main>
+
+        </>
     )
 }
