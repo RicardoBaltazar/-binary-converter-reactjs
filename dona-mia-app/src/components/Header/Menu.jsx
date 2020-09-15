55e7e3724c236dcb02041fe98130b21599a26460
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-15 15:52:27 -0300

    feature - iniciando projeto

---
+++ dona-mia-app/src/components/Header/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <header>
+            <nav>
+                <a href="">PÁGINA INICIAL</a>
+                <a href="">RESERVAS</a>
+                <a href="">NOSSA HISTÓRIA</a>
+                <a href="">MENU</a>
+                <a href="">PEDIDOS ONLINE</a>
+                <a href="">CONTATO</a>
+            </nav>
+        </header>
+    )
+}
