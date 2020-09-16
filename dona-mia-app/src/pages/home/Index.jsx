9dc8b466eec453d532a0b7da25c6b8b53b99bc11
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-16 14:03:11 -0300

    feature - criando section about na home

--- dona-mia-app/src/pages/home/Index.jsx
+++ dona-mia-app/src/pages/home/Index.jsx
@@ -4,6 +4,7 @@ import './home.css'
 const IMG1 = require('../../assets/img-home-1.png')
 const IMG2 = require('../../assets/img-home-2.png')
 const IMG3 = require('../../assets/img-home-3.png')
+const IMG5 = require('../../assets/img-home-5.png')
 
 export default function Home() {
     return (
@@ -15,12 +16,25 @@ export default function Home() {
                     <button>Ver o Menu</button>
 
                     <div className='images-title'>
-                        <img src={IMG1} alt="" />
-                        <img src={IMG2} alt="" />
-                        <img src={IMG3} alt="" />
+                        <img src={IMG1} alt="receita" />
+                        <img src={IMG2} alt="receita" />
+                        <img src={IMG3} alt="receita" />
                     </div>
                 </section>
 
+                <section className='about'>
+                    <img src={IMG5} alt=""/>
+
+                    <div>
+                        <h2>Um sonho que vem cozinhando por anos</h2>
+                        <p>Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. 
+                            É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim 
+                            para adicionar o seu próprio conteúdo e trocar fontes. Sinta-se à vontade 
+                            para arrastar e soltar em qualquer lugar da página. Sou um ótimo lugar para 
+                            você contar a sua história aos seus clientes.</p>
+                        <a href="">Leia Mais</a>
+                    </div>
+                </section>
             </main>
 
         </>
